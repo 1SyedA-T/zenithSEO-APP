5de824cff4890fe383b0a0ed6959828b2e0c9e01
1SyedA-T <faisal.houser47@gmail.com>
2025-09-07 10:35:00 +0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>🚀 Welcome to ZenithSEO-App</h1>
+      <p>This is your AI-powered SEO optimization website.</p>
+    </div>
+  );
+}
